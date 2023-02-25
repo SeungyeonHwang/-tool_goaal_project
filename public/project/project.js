6841681aaf6ba74a06687d1ba8a7d3f1ad834b79
ファン <syhwang.web@gmail.com>
2023-02-25 23:26:40 +0900

    project list / create complete

--- public/project/project.js
+++ public/project/project.js
@@ -1,5 +1,16 @@
 (function ($) {
-    $("#color-options .color-option").click(function() {
+
+    const projectItems = document.querySelectorAll('.project-item');
+    projectItems.forEach(item => {
+        const projectDescription = item.querySelector('.project-description');
+        console.log(projectDescription)
+        item.addEventListener('click', () => {
+            projectDescription.style.display = 'block';
+        });
+    });
+
+
+    $("#color-options .color-option").click(function () {
         $(".color-option").removeClass("active");
         $(this).addClass("active");
         $("#project-color").val($(this).data("color"));
